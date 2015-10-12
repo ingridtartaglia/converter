7628c5170c173ca8a586c8289cb9d2241fb4710c
ingridtartaglia <ingrid.tartaglia@gmail.com>
2015-10-12 17:42:22 -0300

    site

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 var converter = angular.module("converter", []);
 
 converter.controller("converterCtrl", function($scope){
+    $scope.convertFrom = 0;
+    $scope.convertTo = 0;
 
+    $scope.convertValueTo = function(){
+        if ($scope.convertType == 1) {
+            $scope.convertTo = Math.round(($scope.convertFrom * 1.8 + 32) * 100) / 100;
+        }
+        if ($scope.convertType == 2) {
+            $scope.convertTo = Math.round(($scope.convertFrom / 1.609) * 100) / 100;
+        }
+        if ($scope.convertType == 3) {
+            $scope.convertTo = Math.round(($scope.convertFrom * 2.204) * 100) / 100;
+        }
+        if ($scope.convertType == 4) {
+             $scope.convertTo = Math.round(($scope.convertFrom * 0.032) * 100) / 100;
+        }
+        if ($scope.convertType == 5) {
+             $scope.convertTo = Math.round(($scope.convertFrom * 33.814) * 100) / 100;
+        }
+    };
 
-
+    $scope.convertValueFrom = function(){
+        if ($scope.convertType == 1) {
+            $scope.convertFrom = Math.round((($scope.convertTo - 32) * (5/9)) * 100) / 100;
+        }
+        if ($scope.convertType == 2) {
+            $scope.convertFrom = Math.round(($scope.convertTo * 1.609) * 100) / 100;
+        }
+        if ($scope.convertType == 3) {
+            $scope.convertFrom = Math.round(($scope.convertTo / 2.204) * 100) / 100;
+        }
+        if ($scope.convertType == 4) {
+            $scope.convertFrom = Math.round(($scope.convertTo * 30.48) * 100) / 100;
+        }
+        if ($scope.convertType == 5) {
+            $scope.convertFrom = Math.round(($scope.convertTo / 33.814) * 100) / 100;
+        }
+    };
 });
